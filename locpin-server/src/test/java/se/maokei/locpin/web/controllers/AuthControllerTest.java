bdc2f68648e58ecfafe228150f81170ace6d98ee
maokei <rickard.joh@gmail.com>
2022-10-20 01:48:20 +0200

    imports

--- locpin-server/src/test/java/se/maokei/locpin/web/controllers/AuthControllerTest.java
+++ locpin-server/src/test/java/se/maokei/locpin/web/controllers/AuthControllerTest.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.Assert;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import se.maokei.locpin.model.User;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
--- locpin-server/src/test/java/se/maokei/locpin/web/controllers/UserControllerTest.java
+++ locpin-server/src/test/java/se/maokei/locpin/web/controllers/UserControllerTest.java
@@ -5,12 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import se.maokei.locpin.repository.UserRepository;
 
 import javax.persistence.EntityManager;
